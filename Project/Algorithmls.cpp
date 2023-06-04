e1a52b98185f80a6d99457985f545684727e2a10
Mohan <mohan.alane@gmail.com>
2023-06-04 13:15:58 +0530

    C++14 user defined literals

--- Project/Algorithmls.cpp
+++ Project/Algorithmls.cpp
@@ -4,7 +4,7 @@
 int main()
 {
 	std::vector<int> aVector = { 1,2,3,4,5,6,8,9,10 };
-	auto result = std::accumulate(aVector.begin(), aVector.end(), 1, [](int a,int b) {return a * b; });
+	auto result = std::accumulate(aVector.begin(), aVector.end(), 1, [](int a, int b) {return a * b; });
 	std::cout << result;
 	return 0;
 }
